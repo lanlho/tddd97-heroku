9d160c1f4f6e034f490af541bb5d85918e1aa1d8
Kung Kranium <kungkranium@gmail.com>
2020-01-21 21:23:20 +0100

    Saker funkar. Password-matchingr är ok. Knappar stökar

--- Labb1/client.js
+++ Labb1/client.js
@@ -1,28 +1,31 @@
-window.onload = function() {
-  welcomeView = document.getElementById("welcomeView");
-  document.getElementById("body").innerHTML = welcomeView.innerHTML;
+window.onload = function () {
+  welcomeView = document.getElementById('welcomeView');
+  document.getElementById('body').innerHTML = welcomeView.innerHTML;
+
   //Mja, det här funkar ju SO FAR men vi kommer behöva ändra när man ska kunna
   //Stänga ner sidan och fortfarande vara inloggad så att Säga.
-}
+};
 
-  /*passwordVaildate = function() { //Function to write a paragraph DIRECTLY in the page if the passwords dont match
-  var pass1 = document.getElementById("pass").value;
-  var rptpass = document.getElementById("rptpass").value;
-  if (pass1.value != rptpass.value) {
-    document.getElementById("wrongPass").innerHTML = "Passwords don't match";
+function validateLogIn() {
+  var ema = document.getElementById('Logger');
+  var pas = document.getElementById('Passer');
+  if (ema.value === pas.value) {
+    alert('Samesies');
+  } else {
+    alert('Not samesies');
   }
-}*/
-
-validateSignUp = function(){ //Function to give a pop-up alert if passwords dont match
-  //var passe = document.getElementById("pass").value;
-  //var rptpasse = document.getElementById("rptpass").value;
-  //document.getElementById("body").innerHTML = "Hello".innerHTML;
-
-    /*if (passe == rptpasse){
-    alert("Passwords must match!");
-    }/* else{
-      alert("Swag!");
-    }*/
-  
- alert("This is an alert!");
 }
+
+passwordVaildate = function () {
+
+  //Function to write a paragraph DIRECTLY
+  // in the page if the passwords dont match
+  var pass1 = document.getElementById('pass');
+  var rptpass = document.getElementById('rptpass');
+  if (pass1.value !== rptpass.value) {
+    document.getElementById('wrongPass').innerHTML = "Passwords don't match!";
+    //alert('Nu blire knas');
+  } else {
+    document.getElementById('wrongPass').innerHTML = "";
+  }
+};
