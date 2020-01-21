d40720331b998d2601aa57bb56fc3e2282b2afb5
Kung Kranium <kungkranium@gmail.com>
2020-01-21 14:20:23 +0100

    Nja, nåt är ju fel. Men vet inte vad.

--- Labb1/client.js
+++ Labb1/client.js
@@ -4,3 +4,23 @@ window.onload = function() {
   //Mja, det här funkar ju SO FAR men vi kommer behöva ändra när man ska kunna
   //Stänga ner sidan och fortfarande vara inloggad så att Säga.
 }
+
+ function passwordVaildate() { //Function to write a paragraph DIRECTLY in the page if the passwords dont match
+  var pass1 = document.getElementById("pass");
+  var rptpass = document.getElementById("rptpass");
+  if (pass1.value !== rptpass.value) {
+    document.getElementById("wrongPass").innerHTML = "Passwords don't match";
+  }
+}
+
+function validateSignUp() { //Function to give a pop-up alert if passwords dont match
+  var pass1 = document.getElementById("pass");
+  var rptpass = document.getElementById("rptpass");
+  if (pass1.value !== rptpass.value) {
+    alert("Passwords must match! >:-(");
+  }
+  else {
+    alert("coolio");
+    return true;
+  }
+}
