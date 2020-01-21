d9fd1c0efc2b27fec69042ae0301a8b2ec7eca68
Kung Kranium <kungkranium@gmail.com>
2020-01-21 11:01:08 +0100

    Step 2 complete

---
+++ Labb1/client.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  welcomeView = document.getElementById("welcomeView");
+  document.getElementById("body").innerHTML = welcomeView.innerHTML;
+  //Mja, det här funkar ju SO FAR men vi kommer behöva ändra när man ska kunna
+  //Stänga ner sidan och fortfarande vara inloggad så att Säga.
+}
