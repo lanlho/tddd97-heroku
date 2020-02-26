27149b839c97439c824319345518158539883e6c
lanlh134 <lanlh134@student.liu.se>
2020-02-26 18:03:07 +0100

    json responses set, check that it works

--- labb2/server.py
+++ labb2/server.py
@@ -1,19 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import database_helper
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     data = request.get_json()
     found_user = database_helper.find_user(data['email'])
     token = database_helper.match_email_to_password(data['email'], data['password'])
-
     return token
+    if found_user:
+        return jsonify({"success": True, "message": "Successfully signed in.", "data": token})
+    else:
+        return jsonify({"success": False, "message": "Wrong username or password."})
     #return "This is text"
 #-------------------------------------------------------
 @app.route('/sign_up', methods=['POST'])
@@ -40,16 +38,27 @@ def sign_up():
         database_helper.add_user(email,password,first_name,family_name, gender,city,country)
     else:
         return "User already Exist!"
-
     print(data)
-    return("Yolo is cool")
+    is_taken = database_helper.exist_user(email)
+    if (is_taken):
+        return jsonify({"success": False, "message": "User already exists."})
+    else:
+        database_helper.add_user(email, password, firstname, familyname, gender, city, country)
+        return jsonify({"success": True, "message": "Successfully created a new user."})
+
 #---------------------------------------------------------------
+
 @app.route('/sign_out', methods=['POST'])
 def sign_out():
     token = request.headers.get('token')
     print(token)
     return database_helper.sign_out(token)
-    #return ("Fuckin ayy")
+    if token in sign_out:
+        del sign_out[token]
+        return jsonify({"success": True, "message": "Successfully signed out."})
+    else:
+        return jsonify({"success": False, "message": "You are not signed in."})
+
 #--------------------------------------------------------------
 
 @app.route('/change_password', methods=['POST'])
@@ -62,10 +71,17 @@ def change_password():
     except:
         return("Could not read JSON")
     try:
-         return( database_helper.change_password(token, old_password, new_password))
-
+         return(database_helper.change_password(token, old_password, new_password))
     except:
         return("something went really fucking wrong with the pw")
+
+    is_taken = database_helper.exist_user(email)
+    if (is_taken):
+        return jsonify({"success": False, "message": "User already exists."})
+    else:
+        database_helper.add_user(email, password, firstname, familyname, gender, city, country)
+        return jsonify({"success": True, "message": "Successfully created a new user."})
+
 #-----------------------------------------------------------------
 
 @app.route('/get_user_data_by_token', methods=['GET'])
@@ -75,6 +91,7 @@ def get_user_data_by_token():
     except:
         return("Could not access token")
     return database_helper.get_user_data_by_token(token)
+
 #-------------------------------------------------------------------
 
 @app.route('/get_user_data_by_email', methods=['GET'])
@@ -85,6 +102,15 @@ def get_user_data_by_email():
     except:
         return("Could not access header-parameters")
     return database_helper.get_user_data_by_email(token, email)
+
+    if token in get_user_data_by_email:
+        user_data = database_helper.get_user_data(email)
+        if user_data:
+            return jsonify({"success": True, "message": "User data retrieved.", "data": user_data})
+        else:
+            return jsonify({"success": False, "message": "No such user."})
+    else:
+        return jsonify({"success": False, "message": "You are not signed in."})
 
 #------------------------------------------------------------------
 
@@ -101,7 +127,9 @@ def get_user_messages_by_token():
         #print(e)
         StringToReturn += e["sender"] + ":" + e["message"] + "|"
     return(StringToReturn)
+
 #-------------------------------------------------------------------
+
 @app.route('/get_user_messages_by_email', methods=['GET'])
 def get_user_messages_by_email():
     try:
@@ -117,6 +145,19 @@ def get_user_messages_by_email():
         StringToReturn += e["sender"] + ":" + e["message"] + "|"
     return(StringToReturn)
 
+    if token in findEmail:
+        messages = database_helper.get_user_messages_by_email(token)
+        user_exist = database_helper.exist_user(findEmail)
+        if user_exist:
+            return jsonify({"success": True, "message": "User messages retrieved.", "data": messages})
+        else:
+            return jsonify({"success": False, "message": "No such user."})
+    else:
+        return jsonify({"success": False, "message": "You are not signed in."})
+
+
+#-------------------------------------------------------------------
+
 @app.route('/post_message', methods=['POST'])
 def post_message():
     try:
@@ -129,8 +170,18 @@ def post_message():
         message = data["message"]
     except:
         return("Could not get JSON-data")
-
     return database_helper.post_message(token,email,message)
+
+    user_exist = database_helper.exist_user(sending_user)
+    if token in exist_user:
+        receiving_user = logged_in_users[token]
+        if (user_exist):
+            database_helper.post_message(message, receiving_user, sending_user)
+            return jsonify({"success": True, "message": "Message posted"})
+        else:
+            return jsonify({"success": False, "message": "No such user."})
+    else:
+        return jsonify({"success": False, "message": "You are not signed in."})
 
     #return database_helper.get_user_messages_by_email(token, findEmail)
 
