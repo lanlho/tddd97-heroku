0b382e8e01141d6c1f63ea89fb1bdca3456368bf
Lana Lhony <lanlh134@student.liu.se>
2020-03-16 19:03:34 +0100

    Changed IP address

--- twidder/twidder/server.py
+++ twidder/twidder/server.py
@@ -242,5 +242,5 @@ def post_message():
 
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('',80), app, handler_class=WebSocketHandler)
+    http_server = WSGIServer(("127.0.0.1",80), app, handler_class=WebSocketHandler)
     http_server.serve_forever()
